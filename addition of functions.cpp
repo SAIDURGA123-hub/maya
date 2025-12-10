bc0bbd330dea1f08534abbcd877e46e397d151fb
SAIDURGA123-hub <godavarthisaidurga@gmail.com>
2025-12-10 11:33:46 +0530

    Add files via upload

---
+++ addition of functions.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void sum()
+{
+	int a,b,c;
+	printf("Enter two numbers: ");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("%d\n",c);
+}
+int main()
+{
+	sum();
+	printf("Saidurga\n");
+	sum();
+	sum();
+	return 0;
+}
