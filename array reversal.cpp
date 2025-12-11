9dcb67ade83535584b94941094544f38caf6ba7a
SAIDURGA123-hub <godavarthisaidurga@gmail.com>
2025-12-11 13:27:45 +0530

    Add files via upload

---
+++ array reversal.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);   // Read size of array
+    
+    int arr[n];
+    
+    // Read array elements
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Print reversed array
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
+
